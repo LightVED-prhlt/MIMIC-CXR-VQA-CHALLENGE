bebe7cbdc618389392121df6508e138cd187ce1c
whoami <mohamedaasalas@gmail.com>
2025-12-21 02:26:52 +0100

    actualizado el navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,10 +4,8 @@ import CottageOutlinedIcon from "@mui/icons-material/CottageOutlined";
 import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
 import MenuIcon from "@mui/icons-material/Menu";
 import logo from "../../assets/logo3.png";
+import logoside from "../../assets/logo2.png";
 import "./Navbar.css";
-
-
-
 
 import {
   Drawer,
@@ -24,15 +22,12 @@ function Navbar() {
   const [open, setOpen] = useState(false);
 
   const handleClose = () => setOpen(false);
+  const handleOpen = () => setOpen(true);
 
   const SidebarContent = (
     <div className="navbar-container">
       <Sidebar width="270px" showProfile={false}>
-        <Logo
-          component={Link}
-          href="/"
-          img={logo}
-        >
+        <Logo component={Link} href="/" img={logo}>
           My App
         </Logo>
 
@@ -61,20 +56,22 @@ function Navbar() {
 
   return (
     <>
-      {/* Burger button (mobile only) */}
+      {/* Top bar (mobile only, hidden when drawer is open) */}
       {isMobile && !open && (
-        <IconButton
-          onClick={() => setOpen(true)}
-          sx={{
-            position: "fixed",
-            top: 16,
-            left: 16,
-            zIndex: 1300,
-          }}
-        >
-          <MenuIcon />
-        </IconButton>
+        <div className="topbar">
+          <IconButton onClick={handleOpen}>
+            <MenuIcon />
+          </IconButton>
+
+          <div
+            className="topbar-logo-wrapper"
+            onClick={() => (window.location.href = "/")}
+          >
+            <img src={logoside} alt="logo" />
+          </div>
+        </div>
       )}
+
       {/* Mobile drawer */}
       {isMobile ? (
         <Drawer
